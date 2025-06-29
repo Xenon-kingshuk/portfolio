ea1eb8c9de3fdab09d9de0cb1cf709fcc2ab5aa0
Xenon-kingshuk <163967256+Xenon-kingshuk@users.noreply.github.com>
2025-06-29 22:18:20 +0530

    files

---
+++ react-portfolio-master/src/content_option.js
@@ -0,0 +1,168 @@
+const logotext = "Kingshuk Sarkar";
+const meta = {
+    title: "Kingshuk Sarkar",
+    description: "I’m Kingshuk Sarkar, a full stack developer, currently working in india",
+};
+
+const introdata = {
+    title: "I’m Kingshuk Sarkar",
+    animated: {
+        first: "I love coding",
+        second: "I am a full stack developer",
+        third: "I am a tech enthusiast",
+    },
+    description: "Full Stack Developer with a BCA degree, currently pursuing MCA. Always eager to learn and grow in the tech space.",
+    your_img_url: "/profile.png",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "I'm a passionate and results-driven Full Stack Developer with a Bachelor’s degree in Computer Applications (BCA) and currently pursuing my Master’s in Computer Applications (MCA). I specialize in building dynamic, user-friendly web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js) and continuously seek to deepen my technical expertise and industry knowledge.I thrive in both individual and collaborative settings, with a strong focus on writing clean, scalable code and delivering seamless user experiences.",
+};
+const worktimeline = [{
+        jobtitle: "Higher Secondary",
+        where: "Mahbert High School- Siliguri",
+        date: "2021",
+    },
+    {
+        jobtitle: "Bachelor of Computer Applications",
+        where: "University of North Bengal- Siliguri",
+        date: "2024",
+    },
+    {
+        jobtitle: "Masters of Computer Applications",
+        where: "Presidency University- Bengaluru",
+        date: "2026",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Django",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [
+    {
+        title: "UI & UX Design",
+        description: "Creating intuitive interfaces and user-friendly designs that enhance user experience and drive engagement.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Custom mobile app development for iOS and Android platforms with high performance and great user interface.",
+    },
+    {
+        title: "WordPress Design",
+        description: "Professional WordPress themes and custom plugin development tailored to your business needs.",
+    },
+];
+
+
+const dataportfolio = [{
+        img: "/shoping_website.png",
+        description: "ThreadBasket— a shopping website powered by MERN, styled for tomorrow’s wear.",
+        link: "#",
+    },
+    {
+        img: "/weather_app.png",
+        description: "Real-time clarity — the weather app that speaks in code.",
+        link: "#",
+    },
+    {
+        img: "/portfolio_pic.png",
+        description: "JS in action. HTML strong. CSS smooth. Inbox ready.Here's my portfolio.",
+        link: "#",
+    },
+    {
+        img: "/new_projects_coming_soon.png",
+        description: "Creativity stirs quietly — soon, the noise will be undeniable.",
+        link: "#",
+    },
+    {
+        img: "/new_projects_coming_soon.png",
+        description: "The unseen is forming. Get ready for the reveal.",
+        link: "#",
+    },
+    {
+        img: "/new_projects_coming_soon.png",
+        description: "Bold moves ahead — stay tuned for what's coming next.",
+        link: "#",
+    },
+
+    {
+        img: "/new_projects_coming_soon.png",
+        description: "Foundations laid. Focus sharp. A new project is near.",
+        link: "#",
+    },
+    {
+        img: "/new_projects_coming_soon.png",
+        description: "No announcements yet — just action, precision, and purpose.",
+        link: "#",
+    },
+    {
+        img: "/new_projects_coming_soon.png",
+        description: "We build in silence. Results speak louder than words.",
+        link: "#",
+    },
+    {
+        img: "/new_projects_coming_soon.png",
+        description: "We build in silence. Results speak louder than words.",
+        link: "#",
+    },
+    {
+        img: "/new_projects_coming_soon.png",
+        description: "Foundations laid. Focus sharp. A new era is near.",
+        link: "#",
+    },
+    {
+        img: "/new_projects_coming_soon.png",
+        description: "We build in silence. Results speak louder than words.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kingshuksarkar3489@gmail.com",
+    YOUR_FONE: "+91 8967420824",
+    description: "I'd love to hear from you! Have a quesion, idea, or just want to say hi? Feel free to drop a message — I'll get back to you as soon as I can.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_ej2zo7c",
+    YOUR_TEMPLATE_ID: "template_ryivxta",
+    YOUR_USER_ID: "wk5QEcCXsH3qensac",
+};
+
+const socialprofils = {
+    github: "https://github.com/Xenon-kingshuk",
+    facebook: "https://www.facebook.com/share/19DVeBgPJe/",
+    linkedin: "https://www.linkedin.com/in/kingshuk-sarkar-168820212/",
+    instagram: "https://www.instagram.com/_kingshuk_sarkar?igsh=MWNydHN4dWdicHB3bA==",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
